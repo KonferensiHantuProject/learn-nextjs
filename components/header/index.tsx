5aea8d939eb72a31610c6c0fdd88aaf292d23f67
MuhammadNazhimMaulana <nazhimmaulanamuhammad@gmail.com>
2022-11-24 11:01:33 +0700

    Creating First Component

---
+++ components/header/index.tsx
@@ -0,0 +1,14 @@
+import Link from 'next/link'
+
+export default function Header() {
+  return (
+    <header>
+        <ul>
+        <li><Link href="/">Home</Link></li>
+        <li><Link href="/blog">Blog</Link></li>
+        <li><Link href="/users">User</Link></li>
+        {/* <li><a href="/users">User</a></li> */}
+        </ul>
+    </header>
+  )
+}
