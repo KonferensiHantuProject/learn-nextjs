f7956d9d4a3504ae40c156a50254825eb79be833
MuhammadNazhimMaulana <nazhimmaulanamuhammad@gmail.com>
2022-11-26 17:22:39 +0700

    Hotfixing Import

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,6 @@
-import Header from '../components/header'
+import Header from '../components/Header'
 import styles from '../styles/Home.module.css'
-import Footer from '../components/footer'
+import Footer from '../components/Footer'
 
 export default function Home() {
   return (
